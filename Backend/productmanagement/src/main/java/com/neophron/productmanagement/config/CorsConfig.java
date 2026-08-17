ff0f46e8e6b8caac81311547cdfec242781cd4c1
Sanjay D <153883815+Sanjay5274@users.noreply.github.com>
2026-08-17 14:45:43 +0530

    Update package name in CorsConfig.java

--- Backend/productmanagement/src/main/java/com/neophron/productmanagement/config/CorsConfig.java
+++ Backend/productmanagement/src/main/java/com/neophron/productmanagement/config/CorsConfig.java
@@ -1,11 +1,9 @@
-package com.example.productmanagement.config;
+package com.productmanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-package com.productmanagement;
-package com.productmanagement.config;
 
 @Configuration
 public class CorsConfig {
